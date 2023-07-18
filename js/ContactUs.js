a6b1a5c7d7e42b5ce5eefcc4d6b3670a2cd92f1d
Sebastian835 <sebastian.830@outlook.com>
2023-07-18 13:48:25 -0500

    NO SE QUE HICE

--- js/ContactUs.js
+++ js/ContactUs.js
@@ -19,13 +19,59 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  // Validación para el campo firstName al enviar el formulario
-  $("form").on("submit", function (e) {
-    var firstName = $("#firstName").val().trim();
+  $("#contacUS").validate({
+    rules: {
+      firstName: {
+        required: true,
+        minlength: 3,
+      },
+    },
+    messages: {
+      firstName: {
+        required: "Por favor, introduzca su Nombre",
+        minlength: "El nombre debe tener al menos 4 caracteres.",
+      },
+    },
+    submitHandler: function (form) {
+      var firstNameValue = $("#firstName").val().trim();
+      if (firstNameValue === "") {
+        // Crear el modal
+        var modal = $(
+          '<div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true"> \
+          <div class="modal-dialog" role="document"> \
+            <div class="modal-content"> \
+              <div class="modal-header"> \
+                <h5 class="modal-title" id="errorModalLabel">Error en el formulario</h5> \
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"> \
+                  <span aria-hidden="true">&times;</span> \
+                </button> \
+              </div> \
+              <div class="modal-body"> \
+                Por favor, ingrese un valor en el campo Nombre. \
+              </div> \
+              <div class="modal-footer"> \
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button> \
+              </div> \
+            </div> \
+          </div> \
+        </div>'
+        );
 
-    if (firstName === "") {
-      e.preventDefault(); // Evita el envío del formulario
-      alert("Por favor, ingresa tu nombre.");
-    }
+        // Agregar el modal al documento
+        $("body").append(modal);
+
+        // Mostrar el modal
+        modal.modal("show");
+
+        // Remover el modal del documento después de cerrarlo
+        modal.on("hidden.bs.modal", function () {
+          modal.remove();
+        });
+
+        return false;
+      } else {
+        form.submit();
+      }
+    },
   });
 });
