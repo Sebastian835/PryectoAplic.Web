a9a65df80bfa6376c72047570c81aca3371961b7
Nicole352 <75482188+Nicole352@users.noreply.github.com>
2023-07-20 12:12:59 -0500

    Nike.html

---
+++ JS/aboustus.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+    // Cuando el ratón entra en el contenedor
+    $("#cont1").mouseenter(function(){
+        // Agrandamos la imagen con un tamaño mayor al original
+        $(".imag").css({
+            'width': '175px',
+            'height': '225px'
+        });
+    });
+
+    // Cuando el ratón sale del contenedor (volvemos al tamaño original)
+    $("#cont1").mouseleave(function(){
+        $(".imag").css({
+            'width': '150px',
+            'height': '170px'
+        });
+    });
+});
+
+$(document).ready(function(){
+    // Cuando el ratón entra en el contenedor
+    $("#cont2").mouseenter(function(){
+        // Agrandamos la imagen con un tamaño mayor al original
+        $(".imag2").css({
+            'width': '175px',
+            'height': '225px'
+        });
+    });
+
+    // Cuando el ratón sale del contenedor (volvemos al tamaño original)
+    $("#cont2").mouseleave(function(){
+        $(".imag2").css({
+            'width': '150px',
+            'height': '170px'
+        });
+    });
+});
+
+$(document).ready(function(){
+    // Cuando el ratón entra en el contenedor
+    $("#cont3").mouseenter(function(){
+        // Agrandamos la imagen con un tamaño mayor al original
+        $(".imag3").css({
+            'width': '175px',
+            'height': '225px'
+        });
+    });
+
+    // Cuando el ratón sale del contenedor (volvemos al tamaño original)
+    $("#cont3").mouseleave(function(){
+        $(".imag3").css({
+            'width': '150px',
+            'height': '170px'
+        });
+    });
+});
+
+$(document).ready(function(){
+    // Cuando el ratón entra en el contenedor
+    $("#cont4").mouseenter(function(){
+        // Agrandamos la imagen con un tamaño mayor al original
+        $(".imag4").css({
+            'width': '175px',
+            'height': '225px'
+        });
+    });
+
+    // Cuando el ratón sale del contenedor (volvemos al tamaño original)
+    $("#cont4").mouseleave(function(){
+        $(".imag4").css({
+            'width': '150px',
+            'height': '170px'
+        });
+    });
+});
