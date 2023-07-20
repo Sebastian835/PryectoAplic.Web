e97771ec83ab43279cc2992c6a6b0df95d14306b
Nicole352 <75482188+Nicole352@users.noreply.github.com>
2023-07-20 03:29:56 -0500

    JQuery Nike.html

---
+++ JS/Nike.js
@@ -0,0 +1,254 @@
+
+$(document).ready(function () {
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikem1.jpg',
+        '../Imagenes/global1.jfif',
+        '../Imagenes/global2.jfif',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikem2.jpg',
+        '../Imagenes/global3.jfif',
+        '../Imagenes/global4.jfif',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image2');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikem3.jpg',
+        '../Imagenes/global5.jfif',
+        '../Imagenes/global6.jfif',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image3');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikem4.jpg',
+        '../Imagenes/global7.jfif',
+        '../Imagenes/global8.jfif',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image4');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikem5.jpg',
+        '../Imagenes/global9.jfif',
+        '../Imagenes/global10.jfif',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image5');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikem6.jpg',
+        '../Imagenes/global11.jfif',
+        '../Imagenes/global12.jfif',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image6');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikeh1.jpg',
+        '../Imagenes/global13.jpg',
+        '../Imagenes/global14.jpg',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image7');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikeh2.jpg',
+        '../Imagenes/global15.jpg',
+        '../Imagenes/global16.jpg',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image8');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikeh3.jpg',
+        '../Imagenes/global17.jpg',
+        '../Imagenes/global18.jpg',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image9');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikeh4.jpg',
+        '../Imagenes/global19.jpg',
+        '../Imagenes/global20.jpg',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image10');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikeh5.jpg',
+        '../Imagenes/global21.jpg',
+        '../Imagenes/global22.jpg',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image11');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
+$(document).ready(function(){
+    // Array de URLs de las imágenes que quieres mostrar
+    var imageUrls = [
+        '../img/Nikeh6.jpg',
+        '../Imagenes/global23.jpg',
+        '../Imagenes/global24.jpg',
+        // Agrega más URLs si deseas mostrar más imágenes
+    ];
+
+    var currentIndex = 0;
+    var cardImage = $('#card-image12');
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        cardImage.attr('src', imageUrls[currentIndex]);
+    }
+
+    // Cambiar la imagen cada 3 segundos
+    setInterval(changeImage, 3000);
+});
